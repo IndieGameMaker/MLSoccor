0f9565985e3ffcea228a6707a534a7a8fd30e2b5
Lee JaeHyun <myleje@gmail.com>
2020-11-14 13:38:44 +0900

    .

---
+++ Assets/02.Scripts/BallCtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+
+public class BallCtrl : MonoBehaviour
+{
+    public Agent[] players;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("RED_GOAL"))
+        {
+            //Blue + Reward
+            players[0].AddReward(+1.0f);
+            //Red - Reward
+            players[1].AddReward(-1.0f);
+            players[0].EndEpisode();
+            players[1].EndEpisode();
+        }
+
+        if (coll.gameObject.CompareTag("BLUE_GOAL"))
+        {
+            //Blue - Reward
+            players[0].AddReward(-1.0f);
+            //Red + Reward
+            players[1].AddReward(+1.0f);
+            players[0].EndEpisode();
+            players[1].EndEpisode();
+        }        
+
+        transform.localPosition = new Vector3(0.0f, 3.0f, 0.0f);
+        rb.velocity = rb.angularVelocity = Vector3.zero;
+    }
+}
