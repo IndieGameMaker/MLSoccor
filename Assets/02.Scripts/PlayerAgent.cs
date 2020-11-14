df1c5913c7e68bc1957236a04745fe166b3ba9c4
Lee JaeHyun <myleje@gmail.com>
2020-11-14 10:26:11 +0900

    .

--- Assets/02.Scripts/PlayerAgent.cs
+++ Assets/02.Scripts/PlayerAgent.cs
@@ -15,6 +15,12 @@ public class PlayerAgent : Agent
     public TEAM team;
     public GameObject[] players;
 
+    //플레이어의 초기 위치값
+    private Vector3 initPos;
+    //Behaviour Parameters Team ID 추출
+    private BehaviorParameters bps;
+    private Rigidbody rb;
+
     public override void Initialize()
     {
 
