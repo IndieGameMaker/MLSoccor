b4b2d2e03757ffb1e2cc5bc146164f19a7e623a6
Lee JaeHyun <myleje@gmail.com>
2020-11-14 10:17:42 +0900

    .

---
+++ Assets/02.Scripts/PlayerAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Policies;
+
+public class PlayerAgent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
