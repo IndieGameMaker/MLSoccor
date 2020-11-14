5a7a7306599cf7e21250572c3fc2e5cd9310156c
Lee JaeHyun <myleje@gmail.com>
2020-11-14 15:47:52 +0900

    .

---
+++ Assets/02.Scripts/TEST.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator obj = GetComponentInChildren<Animator>();
+        Destroy(obj);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
